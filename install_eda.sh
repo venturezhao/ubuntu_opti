810245e18b457ac2d0bfad5fb8f4d0717d6943c3
wenzhe <venturezhao@gmail.com>
2019-11-19 17:18:28 +0800

    fixed bug: move sh before creating soft link.

--- install_eda.sh
+++ install_eda.sh
@@ -6,5 +6,6 @@ sudo apt -y install lsb-core
 sudo apt -y install libjpeg62
 sudo ln -s /usr/lib/x86_64-linux-gnu/libtiff.so.5 /usr/lib/x86_64-linux-gnu/libtiff.so.3
 sudo ln -s /usr/lib/x86_64-linux-gnu/libmng.so.2 /usr/lib/x86_64-linux-gnu/libmng.so.1
+sudo mv /bin/sh /bin/sh.bk
 sudo ln -s /bin/bash /bin/sh
 
